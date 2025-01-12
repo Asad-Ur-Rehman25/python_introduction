6f3f5e52f61c791e1944156e9a98ed269d49297b
Muhammad Owais <165084627+MuhammadOwais96@users.noreply.github.com>
2025-01-12 21:23:45 +0500

    first-commit

---
+++ index.py
@@ -0,0 +1,259 @@
+# print("Python")
+# # data Type 
+# # boolian
+# # 9number 
+# # string
+
+# num:str="jkj"
+# print(num)
+
+# x=10
+# y=20
+# add=x+y
+# sub=x-y
+# multiply=x*y
+# divide=x/y
+# print(add)
+# print(sub)
+# print(multiply)
+# print(divide)
+
+
+
+
+
+# # value = "adsa" or 0 or "Fallback"
+# # print(value)  # Output: "Fallback" (the first truthy value)
+# # d = 10
+# # c = 20
+# # if d > 5 and c < 40:
+# #     print("Bth conditions are True")
+
+
+
+# # if Else if else topic forth class
+
+# # can = 12
+# # can2 = 20
+# # if can > 2 and can2
+# )
+
+# on="Kelectric"
+# Off="Ups"
+# ligthon=on
+# lightoff=Ups
+# print(on)
+# lighton !
+
+
+# K_Electric = True
+# Ups=False
+# K_Electricoff=False
+# UpsOn=True
+# print(Ups == K_Electric)
+# print(Ups != K_Electric)
+# print(K_Electricoff != UpsOn)
+
+# Electric = True
+# Ups=False
+# if Electric == False or Ups ==True:
+#     print("Ups is On")
+# elif Electric ==True or Ups == False:
+#     print("Ups is Off")
+    
+# chatgpt Code
+# Electric = True
+# Ups = False
+
+# if not Electric or Ups:
+#     print("Ups is On")
+# elif Electric or not Ups:
+#     print("Ups is Off")
+
+
+
+
+
+
+
+
+
+
+# if False:
+#     print("condition is false")
+    
+# if False:
+#     print("con is false")
+# else:
+#     print("condition is true")
+
+# num:int =10
+# # if num1
+# print(num >10)
+
+# num:int =10
+# # if num1
+# print(num <10)
+
+
+# num:int =10
+# # if num1
+# print(num ==10)
+
+# if num >0:
+#     print("num is possitive")
+# else:
+#     print("num is ngetive ")
+
+# number:int =200
+# print(number>0 and number <100)
+
+
+
+# if number>0 and number<100:
+#     print
+
+# name:str="asad"
+# if name=="asad":
+#     print("welcome to asad")
+# else:
+#     print("Not invited you")
+# convension snake casa jis me sub small word likhe hote hai
+# time_of_day:str ="Morning"
+# if time_of_day == "mornig":
+#     print("good Mornig")
+# elif time_of_day =="Morning":
+#     print("good AFternoon")   
+# else:
+#     print("good Night") 
+ 
+
+#  variable
+#  data TypeE 
+# string 
+# intiger 
+# boolian
+# List
+
+
+
+# naames:list[str]=[".Net Developer", "javascriot","pyhton"]
+# print(naames[2])
+
+
+# naames:list[str]=[".Net Developer", "javascriot","pyhton"]
+# # range(naames(0,3))
+# print(range(naames(0,3)))
+
+
+# For loop
+
+# numberrr:list
+# print(*range(0,9))
+
+
+# Simple prompt example
+# name = input("What is your name? ")
+# print(f"Hello, {name}!")
+
+
+
+# asad:int = [1]
+# for i in asad(3):
+#     print(i)
+
+# Correct approach using range
+# for i in range(3):
+#     print(i)
+
+
+# asad: list[int] = [10]
+# for i in range(10):
+#     print(i)
+
+
+
+
+
+# for i in range(5):  # `i` is an int variable
+#     print(f"Current value of i: {i*2}")
+
+
+
+# Table=int(input("Enter Any Number"))
+# for ii in range(11):
+#     print(f"{Table}x{ii}={Table*ii}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Table_of_Num = int(input("Enter Number"))
+# for Table in range(1,11):
+#     print(f"{Table_of_Num} x {Table} = {Table_of_Num*Table}" )
+
+# num:int=2
+# if 10>=num:
+#     print("yes")
+# else:
+#     print("no") 
+
+
+# Percentage= int (input("Enter Your Percentage"))
+# if Percentage >= 80:
+#     print(f"Your result Is A1 Grade")
+# elif Percentage >= 70:
+#     print(f"Your result Is A Grade")
+# elif Percentage >= 60:
+#     print(f"Your result Is B Grade")
+# elif Percentage >= 50:
+#     print(f"Your result Is C Grade")
+# elif Percentage >= 40:
+#     print(f"Your result Is D Grade")
+# elif Percentage >= 30:
+#     print(f"Your result Is E Grade")
+# else:
+#     print(f"Your result Is Fail")
+
+
+
+
+
+
+
+
+
+
+# rang = range(20)
+# rang = range(10,20)
+# rang = range(10,20,2)
+# rang = [1,2,3,4,5,6,7,8,9]
+
+# print(*rang)
+# 1+1=2
+# whileeloop=1
+# while whileeloop < 10:
+#     print(f"number {whileeloop}")
+#     whileeloop+=1
+
+
+# passWOrd:str=""
+# User_input =str(input("enter  any text"))
+# print("YOur Conddition is",passWOrd!=User_input)
+
